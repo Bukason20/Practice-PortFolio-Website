8a88642209c9a8f1cb16faaaaa52a158c56c5a89
Bukason20 <chukwuebukaedward20@gmail.com>
2026-01-09 00:11:45 +0100

    changed error

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/NavBarSection";
 import HeroSection from "./components/HeroSection";
 import AboutSection from "./components/AboutSection";
 import ExperienceSection from "./components/ExperienceSection";
